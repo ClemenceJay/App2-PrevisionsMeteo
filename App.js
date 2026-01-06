5c0002abab5c0b751690778d1813f1bec426fbba
ClemenceJay <clemence.jay@gmail.com>
2026-01-06 14:16:48 +0100

    Récupération de la position

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    const getPosition = async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      let positionActuelle = await Location.getCurrentPositionAsync({});
+      setPosition(positionActuelle);
+    }
+
+    getPosition();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vous êtes ici:</Text>
+      {position ? (
+        <Text>Latitude: {position.coords.latitude} - Longitude: {position.coords.longitude}</Text>
+      ) : (
+        <Text>On attend que la position soit disponible</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
