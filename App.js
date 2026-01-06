e394a7e2f6284c9f6ba37f8130efe6a370d06a7c
ClemenceJay <clemence.jay@gmail.com>
2026-01-06 15:16:21 +0100

    recuperation donnees API

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 
 export default function App() {
 
   const [position, setPosition] = useState(null);
+  const [meteo, setMeteo] = useState(null);
+  const [meteoIconeUrl, setMeteoIconeUrl] = useState(null);
+  const apiKey = Constants.expoConfig.extra.openWeatherApiKey;
 
   useEffect(() => {
     const getPosition = async() => {
@@ -17,6 +21,29 @@ export default function App() {
     getPosition();
   }, []);
 
+  useEffect(() => {
+
+    const getMeteo = async() => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+      try {
+        const response = await fetch(url);
+        const dataMeteo = await response.json();
+        setMeteo(dataMeteo);
+
+        let iconeURL = `https://openweathermap.org/img/wn/${dataMeteo.weather[0].icon}@2x.png`;
+        setMeteoIconeUrl(iconeURL);
+      } catch (error) {
+        console.log("Erreur API météo :", error);
+      }
+    }
+
+    getMeteo(); 
+
+  }, [position]);
+
   return (
     <View style={styles.container}>
       <Text>Vous êtes ici:</Text>
@@ -25,6 +52,20 @@ export default function App() {
       ) : (
         <Text>On attend que la position soit disponible</Text>
       )}
+      {meteo ? (
+        <View>
+          <Text>Vous êtes ici : {meteo.name}</Text>
+          <Text>Il fait : {meteo.main.temp}°C</Text>
+          <Text>Le temps : {meteo.weather[0].description}</Text>
+          <Image source={{uri: meteoIconeUrl}} style={{width: 50, height: 50}}/>
+        </View>
+      ) : (
+        <Text>On attend le retour API</Text>
+      )}
+      <Button
+        title='relaod'
+        color='#3e3f2cff'
+        onPress={() => setPosition(null)}/>
       <StatusBar style="auto" />
     </View>
   );
